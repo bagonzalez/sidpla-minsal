b4cebb18b84a17cba4ed2e9605ddfb5ab297f1d6
Bruno González <bagonzalez@bagonzalez-PC.lan>
2011-08-03 15:51:06 -0600

    Creando modelo de plantilla por defecto, y eliminando variables de ingreso en URI principal

--- MinSal/SidPlaBundle/Controller/DefaultController.php
+++ MinSal/SidPlaBundle/Controller/DefaultController.php
@@ -8,8 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('MinSalSidPlaBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('MinSalSidPlaBundle:Default:index.html.twig');
     }
 }
