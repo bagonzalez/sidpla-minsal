586e2f5343b0fdd8d5a9a7703e4f3414a7a53432
Bruno González <bagonzalez@bagonzalez-PC.lan>
2011-08-05 19:01:55 -0600

    Se crea los archivos necesarios como formularios, plantilla de formulario y action para poder ingresar una nueva opcion al sistema, y guardarla en la base de datos.

--- MinSal/SidPla/AdminBundle/Controller/AccionAdminOpcionesController.php
+++ MinSal/SidPla/AdminBundle/Controller/AccionAdminOpcionesController.php
@@ -1,11 +1,42 @@
 <?php
 
+/*
+  SIDPLA - MINSAL
+  Copyright (C) 2011  Bruno González   e-mail: bagonzalez.sv EN gmail.com
+  Copyright (C) 2011  Universidad de El Salvador
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+  
+ */
+
+/**
+ * Description of AccionAdminOpcionesController
+ *
+ * @author Bruno González
+ */
+
 namespace MinSal\SidPla\AdminBundle\Controller;
 
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use MinSal\SidPla\AdminBundle\EntityDao\OpcionSistemaDao;
+use MinSal\SidPla\AdminBundle\Form\Type\OpcionSistemaType;
+use MinSal\SidPla\AdminBundle\Entity\OpcionSistema;
+
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 
 class AccionAdminOpcionesController extends Controller
@@ -18,11 +49,38 @@ class AccionAdminOpcionesController extends Controller
      *  mensajes de exito/fracaso del sistema.
      */
 	
-	public function addOcpAction()
+	public function addOcpAction(Request $peticion)
 	{
-		$opcDao = new OpcionSistemaDao($this->getDoctrine()->getEntityManager());	
-		$mensajesSistema = $opcDao->addOpcion('opcionPrueba', 'descripcion');	 
-	
-	    return new Response($mensajesSistema[0].' '.$mensajesSistema[1] );
+            $opc=new OpcionSistema(); 
+            $form = $this->createForm(new OpcionSistemaType() , $opc);
+            
+            if ($peticion->getMethod() == 'POST') {
+                $form->bindRequest($peticion);
+
+                if ($form->isValid()) {
+                    $opcDao = new OpcionSistemaDao($this->getDoctrine()->getEntityManager());                
+                    $mensajesSistema = $opcDao->addOpcion($opc);	                     
+                    return new Response($mensajesSistema[0].' '.$mensajesSistema[1] );                    
+                }
+            }
+            return $this->redirect($this->generateUrl('MinSalSidPlaAdminBundle_homepage'));	    
 	}
+        
+        /*
+         * Crea un nuevo formulario, para ser utilizado, para crear una Nueva Opcion del sistema.
+         */
+        
+        
+        public function nuevaOpcAction()
+	{
+            $opc=new OpcionSistema();            
+            
+            $form = $this->createForm(new OpcionSistemaType() , $opc);
+            return $this->render('MinSalSidPlaAdminBundle:Default:opcionFormTemplate.html.twig', 
+                    array('form' => $form->createView(),  ));
+            
+	}
+        
+        
+        
 }
