252cbf10ab4312391acd525d16ddf2f02d1cf308
Bruno González <bagonzalez@bagonzalez-PC.lan>
2011-08-23 06:42:42 -0600

    Mostar Unidad Organizativa

--- MinSal/SidPla/AdminBundle/Controller/AccionAdminUnidadOrgController.php
+++ MinSal/SidPla/AdminBundle/Controller/AccionAdminUnidadOrgController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use MinSal\SidPla\AdminBundle\EntityDao\UnidadOrganizativaDao;
+use MinSal\SidPla\AdminBundle\Entity\UnidadOrganizativa;
+
 /**
  * Description of AccionAdminUnidadOrgController
  *
@@ -14,8 +17,48 @@ use Symfony\Component\HttpFoundation\Request;
 class AccionAdminUnidadOrgController extends Controller {
     
     public function consultarUnidadesOrgAction(){
-        return new Response('hola soy consultar unidad org');        
+        
+        $opciones=$this->getRequest()->getSession()->get('opciones');
+       
+        $unidadOrgDao=new UnidadOrganizativaDao($this->getDoctrine());        
+        $unidadesOrg=$unidadOrgDao->getUnidadesOrg();        
+        
+        return $this->render('MinSalSidPlaAdminBundle:UnidadOrganizativa:showAllUnidadesOrganizativas.html.twig'
+                , array('opciones' => $opciones, 'unidadesorg' => $unidadesOrg));              
     } 
+    
+    public function consultarUnidadesOrgJSONAction(){
+        
+        $unidadOrgDao=new UnidadOrganizativaDao($this->getDoctrine());        
+        $unidadesOrg=$unidadOrgDao->getUnidadesOrg(); 
+        
+        $numfilas=count($unidadesOrg);  
+            
+            $uni=new UnidadOrganizativa();
+            $i=0;
+            
+            foreach ($unidadesOrg as $uni) {
+                $rows[$i]['id']= $uni->getIdUnidadOrg();
+                $rows[$i]['cell']= array($uni->getIdUnidadOrg(),
+                                         $uni->getNombreUnidad(),
+                                         $uni->getDescripcionUnidad());    
+                $i++;
+            }
+            
+            $datos=json_encode($rows);            
+            
+            
+            $jsonresponse='{
+               "page":"1",
+               "total":"1",
+               "records":"'.$numfilas.'", 
+               "rows":'.$datos.'}';
+            
+            
+            $response=new Response($jsonresponse);              
+            return $response;            
+        
+    }
     
     public function ingresoNuevaUnidadesOrgAction(){
         $opciones=$this->getRequest()->getSession()->get('opciones'); 
