585d39f72829672b88f26a18db56f011ce6bbea4
Edwin <edwinrodriboni@hotmail.com>
2011-08-25 22:05:25 -0600

    Anexo la parte de mensaje correo template pero o me funciona la parte de agregar y modificar solo la parte de borrar.

---
+++ MinSal/SidPla/AdminBundle/Controller/AccionAdminMenCorreTempController.php
@@ -0,0 +1,113 @@
+<?php
+
+
+namespace MinSal\SidPla\AdminBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+use MinSal\SidPla\AdminBundle\EntityDao\MensajeCorreTempDao;
+use MinSal\SidPla\AdminBundle\Entity\MensajeCorreTemp;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AccionAdminMenCorreTempController
+ *
+ * @author edwin
+ */
+class AccionAdminMenCorreTempController extends Controller {
+    //put your code here
+
+         
+    public function consultarMenCorrtempAction(){
+        
+        $opciones=$this->getRequest()->getSession()->get('opciones');
+       
+        $MensajeCoDao=new MensajeCorreTempDao($this->getDoctrine());        
+        $MensajeCorreT=$MensajeCoDao->getMensaTem();        
+        
+        return $this->render('MinSalSidPlaAdminBundle:MensajeCorreoTemplate:manttMensajesCorreoTemForm.html.twig'
+                , array('opciones' => $opciones, 'MensajeCorreT' => $MensajeCorreT));              
+    } 
+    
+     public function consultarMenCorrtempJSONAction(){
+        
+       
+         $MensajeCoDao=new MensajeCorreTempDao($this->getDoctrine());        
+        $MensajeCorreT=$MensajeCoDao->getMensaTem();  
+         
+        
+        $numfilas=count($MensajeCorreT);  
+            
+            $uni=new MensajeCorreTemp();
+            $i=0;
+            
+            foreach ($MensajeCorreT as $uni) {
+                $rows[$i]['id']= $uni->getMencortemCodigo();
+                $rows[$i]['cell']= array($uni->getMencortemCodigo(),
+                                         $uni->getMencortemTexto(),
+                                         $uni->getMencortemNombre());    
+                $i++;
+            }
+            
+            $datos=json_encode($rows);            
+            
+            
+            $jsonresponse='{
+               "page":"1",
+               "total":"1",
+               "records":"'.$numfilas.'", 
+               "rows":'.$datos.'}';
+            
+            
+            $response=new Response($jsonresponse);              
+            return $response;            
+        
+    }
+    
+   /*
+         * Opciones de mantenimiento de roles
+         * Eliminar, agregar, editar
+         * 
+         */
+        
+        
+   
+    
+    public function manttMenCorrtempEdicionAction(){
+            
+            $request=$this->getRequest();
+            
+            $nombreRol=$request->get('texto');
+            $funciones=$request->get('nombre');            
+            $id=$request->get('id');
+            
+            $operacion=$request->get('oper'); 
+            
+            $rolDao=new MensajeCorreTempDao($this->getDoctrine());
+            
+            if($operacion=='edit'){                
+                $rolDao->editMensaTem($nombreRol, $funciones, $id);
+            }
+            
+            if($operacion=='del'){
+                $rolDao->delMensaTem($id);        
+            }
+            
+            if($operacion=='add'){
+                $rolDao->addMensaTem($nombreRol, $funciones);
+            }
+             
+            return new Response("{sc:true,msg:''}"); 
+            
+        }      
+    
+}
+
+?>
---
+++ MinSal/SidPla/AdminBundle/EntityDao/MensajeCorreTempDao.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MensajeCorreTempDao
+ *
+ * @author edwin
+ */
+
+namespace MinSal\SidPla\AdminBundle\EntityDao;
+
+
+
+
+class MensajeCorreTempDao {
+    //put your code here
+
+    var $doctrine;
+    var $repositorio;
+    var $em;    
+	
+    function __construct($doctrine){ 
+        $this->doctrine=$doctrine;      	
+        $this->em=$this->doctrine->getEntityManager();
+        $this->repositorio=$this->doctrine->getRepository('MinSalSidPlaAdminBundle:MensajeCorreTemp');
+    } 
+    
+  
+    
+    public function getMensaTem() {	    
+        $mensajes=$this->repositorio->findAll();
+        return $mensajes;
+    }
+  
+    /*
+     *  Almacena un rol ingresado en el sistema
+     */
+    
+    public function addMensaTem($nombreRol, $funciones) {
+        
+        $MensatemSistema=new MensajeCorreTemp();
+        
+        $MensatemSistema->setMencortemTexto($nombreRol);
+        $MensatemSistema->setMencortemNombre($funciones);
+	    
+        $this->em->persist($MensatemSistema);
+        $this->em->flush();	    
+        $matrizMensajes = array('El proceso de almacenar el mensaje correo ha termino con exito', 'Rol '.$MensatemSistema->getMencortemTexto());
+
+        return $matrizMensajes;
+    }
+    
+    public function editMensaTem($nombreRol, $funciones, $id){
+            
+            $MensaTemp=new MensajeCorreTemp();            
+            $MensaTemp=$this->repositorio->find($id);
+            
+            if(!$MensaTemp){
+                throw $this->createNotFoundException('No se encontro rol con ese id '.$id);
+            }
+            
+            $MensaTemp->setMencortemTexto($nombreRol);
+            $MensaTemp->setMencortemNombre($funciones);
+            
+            $this->em->flush();            
+            $matrizMensajes = array('El proceso de almacenar termino con exito', 'Rol '.$MensaTemp->getMencortemTexto());
+ 
+            return $matrizMensajes;
+        }
+        
+         /*
+         * eliminar rol
+         */
+        
+        
+        public function delMensaTem($id){            
+            
+            $MensaTemp=$this->repositorio->find($id);
+            
+            if(!$MensaTemp){
+                throw $this->createNotFoundException('No se encontro el mensaje con ese  id '.$id);
+            }
+            
+            $this->em->remove($MensaTemp);
+            $this->em->flush();
+            
+            $matrizMensajes = array('El proceso de eliminar el mensaje correo termino con exito', 'Rol '.$MensaTemp->getMencortemTexto());
+ 
+            return $matrizMensajes;
+        }
+    
+    
+    
+    
+    
+    
+}
+
+?>
