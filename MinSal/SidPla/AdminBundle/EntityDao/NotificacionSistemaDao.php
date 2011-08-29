f3660d65e7d42275be63f880e83f6c5801d47fa1
KarencitaPenate <karensita_2410@hotmail.com>
2011-08-28 21:45:37 -0600

    Mantenimiento Notificacion Terminado. Correcciones avisen

--- MinSal/SidPla/AdminBundle/EntityDao/NotificacionSistemaDao.php
+++ MinSal/SidPla/AdminBundle/EntityDao/NotificacionSistemaDao.php
@@ -3,6 +3,7 @@
 namespace MinSal\SidPla\AdminBundle\EntityDao;
 
 use MinSal\SidPla\AdminBundle\Entity\NotificacionSistema;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class NotificacionSistemaDao {
 
@@ -26,14 +27,18 @@ class NotificacionSistemaDao {
         return $notificaciones;
     }
 
+    /*
+     * Agregar Notificacion del Sistema
+     */
+
     public function addNotiSistema($nombreNoti, $tipoMensajeNoti, $mensajeNoti) {
 
         $notificacionsistema = new NotificacionSistema();
-        
+
         $notificacionsistema->setNombreNoti($nombreNoti);
         $notificacionsistema->setTipoMensajeNoti($tipoMensajeNoti);
         $notificacionsistema->setMensajeNoti($mensajeNoti);
-             
+
         $this->em->persist($notificacionsistema);
         $this->em->flush();
         $matrizMensajes = array('El proceso de almacenar notificacion del sistema termino con exito',
@@ -41,6 +46,48 @@ class NotificacionSistemaDao {
 
         return $matrizMensajes;
     }
+
+    /*
+     * Elimina Notificacion del sistema
+     */
+
+    public function delNotiSistema($codigo) {
+
+        $notificacionSistema = $this->repositorio->find($codigo);
+
+        if (!$notificacionSistema) {
+            throw $this->createNotFoundException('No se encontro rol con ese id ' . $codigo);
+        }
+
+        $this->em->remove($notificacionSistema);
+        $this->em->flush();
+
+        $matrizMensajes = array('El proceso de eliminar termino con exito');
+
+        return $matrizMensajes;
+    }
+
+    /*
+     * Actualiza Notificacion Sistema
+     */
+
+    public function editNotiSistema($codigo, $nombreNoti, $mensajeNoti, $tipoMensajeNoti) {
+
+        $notificacionSistema = $this->repositorio->find($codigo);
+
+        if (!$notificacionSistema) {
+            throw $this->createNotFoundException('No se encontro rol con ese id ' . $codigo);
+        }
+        
+        $notificacionSistema->setMensajeNoti($mensajeNoti);
+        $notificacionSistema->setNombreNoti($nombreNoti);
+        $notificacionSistema->setTipoMensajeNoti($tipoMensajeNoti);
+
+        $this->em->flush();
+        $matrizMensajes = $codigo;
+    
+            return $matrizMensajes;
+        }
 
 }
 
