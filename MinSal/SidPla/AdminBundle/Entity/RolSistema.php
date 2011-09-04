663857c39358e701ede45609d99af34faff52f90
Bruno González <bagonzalez@bagonzalez-PC.lan>
2011-09-03 20:46:43 -0600

    arreglando dependencia para ArrayColecction

--- MinSal/SidPla/AdminBundle/Entity/RolSistema.php
+++ MinSal/SidPla/AdminBundle/Entity/RolSistema.php
@@ -3,6 +3,7 @@
 namespace MinSal\SidPla\AdminBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * MinSal\SidPla\AdminBundle\Entity\RolSistema
