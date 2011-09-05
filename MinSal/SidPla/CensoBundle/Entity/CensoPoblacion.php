da5c1d366c438a8fd4cc506aa66c01c4f899076c
Bruno González <bagonzalez@bagonzalez-PC.lan>
2011-09-04 19:19:09 -0600

    Creando relacion M:M para censopoblacion y categoria censo

--- MinSal/SidPla/CensoBundle/Entity/CensoPoblacion.php
+++ MinSal/SidPla/CensoBundle/Entity/CensoPoblacion.php
@@ -28,6 +28,15 @@ class CensoPoblacion
      * @ORM\JoinColumn(name="pao_codigo", referencedColumnName="pao_codigo")
      */
     private $pao;
+    
+     /**
+     * @ORM\ManyToMany(targetEntity="CategoriaCenso")
+     * @ORM\JoinTable(name="sidpla_censo_categoria",
+     *      joinColumns={@ORM\JoinColumn(name="censopoblacion_codigo", referencedColumnName="censopoblacion_codigo")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="catcen_codigo", referencedColumnName="catcen_codigo")}
+     *      )
+     */
+    private $categoriasCenso;
 
 
     /**
@@ -68,5 +77,30 @@ class CensoPoblacion
     public function getPao()
     {
         return $this->pao;
+    }
+    
+    public function __construct()
+    {
+        $this->categoriasCenso = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Add categoriasCenso
+     *
+     * @param MinSal\SidPla\CensoBundle\Entity\CategoriaCenso $categoriasCenso
+     */
+    public function addCategoriasCenso(\MinSal\SidPla\CensoBundle\Entity\CategoriaCenso $categoriasCenso)
+    {
+        $this->categoriasCenso[] = $categoriasCenso;
+    }
+
+    /**
+     * Get categoriasCenso
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getCategoriasCenso()
+    {
+        return $this->categoriasCenso;
     }
 }
