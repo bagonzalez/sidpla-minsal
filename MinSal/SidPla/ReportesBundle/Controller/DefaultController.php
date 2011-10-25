763c37debf5b90be07cbc08a105b37e6478848cc
yeni <yeni@yeni.lan>
2011-10-24 21:57:52 -0600

    Creacion de dos paquetes y sus entidades: MinSalSidPlaRRMedicoBundle:     resource: "@MinSalSidPlaRRMedicoBundle/Resources/config/routing.yml"     prefix:   /SidPla/rrmed
    MinSalSidPlaDepUniBundle:
        resource: "@MinSalSidPlaDepUniBundle/Resources/config/routing.yml"
        prefix:   /SidPla/depuni

--- MinSal/SidPla/ReportesBundle/Controller/DefaultController.php
+++ MinSal/SidPla/ReportesBundle/Controller/DefaultController.php
@@ -96,52 +96,6 @@ class DefaultController extends Controller
   
         return $this->getResponse();
     }
-    //Reporte Elementos Infraestructura Evaluada
-    public function reporteInfraEvaluadaAction()
-    {
-            $request=$this->getRequest();
-            $JustiPao=$request->get('justificacion');            
-            $id=$request->get('id');           
-            
-            try {
-                
-                $compileManager = new JavaClass("net.sf.jasperreports.engine.JasperCompileManager");                
-                $report = $compileManager->compileReport(__DIR__."/../Resources/jasperReports/reportJustificacion/reporteJustificacion.jrxml");                
-                $fillManager = new JavaClass("net.sf.jasperreports.engine.JasperFillManager");
-
-                $params = new Java("java.util.HashMap");
-                $params->put("idJustificacion", new java("java.lang.Integer", $id));
-
-                $Conn=$this->crearConexion();    
-
-                $jasperPrint = $fillManager->fillReport($report, $params, $Conn);
-                $outputPath = realpath(".")."/"."output.pdf";
-
-                $exportManager = new JavaClass("net.sf.jasperreports.engine.JasperExportManager");
-                $exportManager->exportReportToPdfFile($jasperPrint, $outputPath);
-            
-                header("Content-type: application/pdf");
-                readfile($outputPath);
-                unlink($outputPath);                
-                $Conn->close();
-                
-                $this->getResponse()->clearHttpHeaders();
-                $this->getResponse()->setHttpHeader('Pragma: public', true);
-                $this->getResponse()->setContentType('application/pdf');                
-                $this->getResponse()->sendHttpHeaders();
-                
-           }
-          catch( Exception $ex ) {
-            print $ex->getCause();
-            if( $Conn != null ) {
-              $Conn->close();
-            }
-              throw $ex;
-          }
-  
-        return $this->getResponse();
-    }
-    
     
     public function reporteActividadesAtrasadasAction()
     {
@@ -187,4 +141,100 @@ class DefaultController extends Controller
   
         return $this->getResponse();
     }
+    
+    //Reporte informacion general de las inidades organizativas.
+    public function reporteInfoGeneralAction()
+    {
+          //$request=$this->getRequest();
+          //  $JustiPao=$request->get('justificacion');            
+          //$id=$request->get('id');           
+          $id="Reporte";
+            try {
+                // obtiene la ruta del reporte 
+                $compileManager = new JavaClass("net.sf.jasperreports.engine.JasperCompileManager");                
+                $report = $compileManager->compileReport(__DIR__."/../Resources/jasperReports/reportInfoGeneralUniOrg/ReporteInfoGeneral.jrxml");                
+//                $fillManager = new JavaClass("net.sf.jasperreports.engine.JasperFillManager");
+
+                $params = new Java("java.util.HashMap");
+                $params->put("nomrepot", new java("java.lang.String", $id));
+
+                $Conn=$this->crearConexion();    
+
+                $jasperPrint = $fillManager->fillReport($report, $params, $Conn);
+                $outputPath = realpath(".")."/"."output.pdf";
+
+                $exportManager = new JavaClass("net.sf.jasperreports.engine.JasperExportManager");
+                $exportManager->exportReportToPdfFile($jasperPrint, $outputPath);
+            
+                header("Content-type: application/pdf");
+                readfile($outputPath);
+                unlink($outputPath);                
+                $Conn->close();
+                
+                $this->getResponse()->clearHttpHeaders();
+                $this->getResponse()->setHttpHeader('Pragma: public', true);
+                $this->getResponse()->setContentType('application/pdf');                
+                $this->getResponse()->sendHttpHeaders();
+                
+           }
+          catch( Exception $ex ) {
+            print $ex->getCause();
+            if( $Conn != null ) {
+              $Conn->close();
+            }
+              throw $ex;
+          }
+  
+        return $this->getResponse();
+    }
+     
+    //Reporte Elementos Infraestructura Evaluada
+    public function reporteInfraEvaluadaAction()
+    {
+            $request=$this->getRequest();
+            $JustiPao=$request->get('infraEvaluada');            
+            $id=$request->get('id');           
+            
+            try {
+                //compilado reporte y cargando en memoria
+                $compileManager = new JavaClass("net.sf.jasperreports.engine.JasperCompileManager");                
+                $report = $compileManager->compileReport(__DIR__."/../Resources/jasperReports/reportInfraEvaluada/reportInfraEvaluada.jrxml");                
+                $fillManager = new JavaClass("net.sf.jasperreports.engine.JasperFillManager");
+                //pasando parametros al reporte
+                $params = new Java("java.util.HashMap");
+                $params->put("infraEvaluada", new java("java.lang.Integer", $id));//asignando valor al parametro
+
+                $Conn=$this->crearConexion();    
+                //se llena el reporte con la informacion y parametros 
+                $jasperPrint = $fillManager->fillReport($report, $params, $Conn);
+                $outputPath = realpath(".")."/"."output.pdf"; //mostrar el reporte en pdf
+
+                $exportManager = new JavaClass("net.sf.jasperreports.engine.JasperExportManager");
+                $exportManager->exportReportToPdfFile($jasperPrint, $outputPath);
+            
+                header("Content-type: application/pdf");
+                readfile($outputPath);
+                unlink($outputPath);                
+                $Conn->close();
+                
+                $this->getResponse()->clearHttpHeaders();
+                $this->getResponse()->setHttpHeader('Pragma: public', true);
+                $this->getResponse()->setContentType('application/pdf');                
+                $this->getResponse()->sendHttpHeaders();
+                
+           }
+          catch( Exception $ex ) {
+            print $ex->getCause();
+            if( $Conn != null ) {
+              $Conn->close();
+            
+              
+              
+            }
+              throw $ex;
+          }
+  
+        return $this->getResponse();
+    }
+    
 }
