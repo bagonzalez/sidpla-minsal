682e478ad1d4f3748a4669f1e519fb9363977447
KarencitaPenate <karensita_2410@hotmail.com>
2011-10-31 22:38:55 -0600

    Default Controller de reportes

--- MinSal/SidPla/ReportesBundle/Controller/DefaultController.php
+++ MinSal/SidPla/ReportesBundle/Controller/DefaultController.php
@@ -1,25 +1,5 @@
 <?php
 
-/*
-  SIDPLA - MINSAL
-  Copyright (C) 2011  Bruno González   e-mail: bagonzalez.sv EN gmail.com
-  Copyright (C) 2011  Universidad de El Salvador
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- */
-
 namespace MinSal\SidPla\ReportesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -87,8 +67,7 @@ class DefaultController extends Controller {
 
         return $this->getResponse();
     }
-    
-    //Reporte Elementos Infraestructura Evaluada
+
     public function reporteMatrizdeObjetivosyResultadosAction() {
         $request = $this->getRequest();
         $tipoUnidad = $request->get('tipoUnidad');
@@ -127,6 +106,40 @@ class DefaultController extends Controller {
             throw $ex;
         }
 
+        return $this->getResponse();
+    }
+
+    public function reporteConsolidadoNivelCentralAction() {
+       $request = $this->getRequest();
+       $tipoUnidad = $request->get('tipoUnidad');
+       $anio = $request->get('anio');
+     try {
+         $compileManager = new JavaClass("net.sf.jasperreports.engine.JasperCompileManager");
+         $report = $compileManager->compileReport(__DIR__ . "/../Resources/jasperReports/reportConsolidadosDep/consolidadocentral.jrxml");
+         $fillManager = new JavaClass("net.sf.jasperreports.engine.JasperFillManager");
+         $params = new Java("java.util.HashMap");
+         $params->put("tipoUnidad", new java("java.lang.String", $tipoUnidad)); //asignando valor al parametro
+         $Conn = $this->crearConexion();
+         $jasperPrint = $fillManager->fillReport($report, $params, $Conn);
+         $outputPath = realpath(".") . "/" . "output.pdf"; //mostrar el reporte en pdf
+         $exportManager = new JavaClass("net.sf.jasperreports.engine.JasperExportManager");
+         $exportManager->exportReportToPdfFile($jasperPrint, $outputPath);
+         header("Content-type: application/pdf");
+         readfile($outputPath);
+         unlink($outputPath);
+         $Conn->close();
+         $this->getResponse()->clearHttpHeaders();
+         $this->getResponse()->setHttpHeader('Pragma: public', true);
+         $this->getResponse()->setContentType('application/pdf');
+         $this->getResponse()->sendHttpHeaders();
+        } 
+        catch (Exception $ex) {
+            print $ex->getCause();
+            if ($Conn != null) {
+                $Conn->close();
+            }
+        throw $ex;
+        }
         return $this->getResponse();
     }
 
