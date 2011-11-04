e43539e3548f8fa754c5e9174257554f4464e999
KarencitaPenate <karensita_2410@hotmail.com>
2011-11-04 00:15:12 -0600

    Algunos Cambios

--- MinSal/SidPla/AdminBundle/Controller/AccionAdminUnidadOrgController.php
+++ MinSal/SidPla/AdminBundle/Controller/AccionAdminUnidadOrgController.php
@@ -1,28 +1,4 @@
 <?php
-
-
-/*
-
-  SIDPLA - MINSAL
-  Copyright (C) 2011  Bruno González   e-mail: bagonzalez.sv EN gmail.com
-  Copyright (C) 2011  Universidad de El Salvador
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
- */
-
 namespace MinSal\SidPla\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -39,11 +15,6 @@ use MinSal\SidPla\AdminBundle\EntityDao\EmpleadoDao;
 use MinSal\SidPla\AdminBundle\Entity\Empleado;
 use MinSal\SidPla\AdminBundle\EntityDao\InformacionGeneralDao;
 
-/**
- * Description of AccionAdminUnidadOrgController
- *
- * @author bagonzalez
- */
 class AccionAdminUnidadOrgController extends Controller {
 
     public function consultarUnidadesOrgAction() {
@@ -81,6 +52,11 @@ class AccionAdminUnidadOrgController extends Controller {
                 '',
                 $infogeneral->getDireccion(),
                 $infogeneral->getTelefono());
+            if($uni->getResponsable()!=NULL){
+                $empleadoDao= new EmpleadoDao($this->getDoctrine());
+                $empleado= $empleadoDao->getEmpleado($uni->getResponsable());
+                $rows[$i]['cell'][3]=$empleado->getPrimerNombre()." ".$empleado->getSegundoNombre()." ".$empleado->getPrimerApellido()." ".$empleado->getSegundoApellido();
+            }
             $i++;
         }
 
--- MinSal/SidPla/GesObjEspEntControlBundle/EntityDao/ObjespTemplateDao.php
+++ MinSal/SidPla/GesObjEspEntControlBundle/EntityDao/ObjespTemplateDao.php
@@ -6,51 +6,40 @@ use MinSal\SidPla\GesObjEspEntControlBundle\Entity\ObjespTemplate;
 use MinSal\SidPla\GesObjEspEntControlBundle\Entity\ResEspTemplate;
 
 class ObjespTemplateDao {
+
     var $doctrine;
     var $repositorio;
-    var $em;    
-	
-    function __construct($doctrine){ 
-        $this->doctrine=$doctrine;      	
-        $this->em=$this->doctrine->getEntityManager();
-        $this->repositorio=$this->doctrine->getRepository('MinSalSidPlaGesObjEspEntControlBundle:ObjespTemplate');
-    } 
-    
-    public function getObjetivoTemplate($id) {	    
-        $objetivotemplate=$this->repositorio->find($id);
+    var $em;
+
+    function __construct($doctrine) {
+        $this->doctrine = $doctrine;
+        $this->em = $this->doctrine->getEntityManager();
+        $this->repositorio = $this->doctrine->getRepository('MinSalSidPlaGesObjEspEntControlBundle:ObjespTemplate');
+    }
+
+    public function getObjetivoTemplate($id) {
+        $objetivotemplate = $this->repositorio->find($id);
         return $objetivotemplate;
     }
-    
-   
-    public function agregarResulEsperadoTemplate($resEspeDesc,$resEspNomencl,$resEspIndicador, $idObjetivo) {
-         
-      
-           $objetivoespecificoAux=new ObjespTemplate();
-           $objetivoespecificoAux=$this->getObjetivoTemplate($idObjetivo); 
-       // $tipometa=1;
-        
-           //$resEspNomencl="gfdsfsdf";
-       // $resEspMetAnual=100;
-       // $resEspDescMetAnual="ttttt";
-       // $resEspResponsable="neto";
-         $objResulesperado=new ResEspTemplate();
-         $objResulesperado->setResEspTemplDescripcion($resEspeDesc);                         
-         $objResulesperado->setResEspTemplIndicador($resEspIndicador);                         
-         $objResulesperado->setIdObjEspecTempl($objetivoespecificoAux);
-                 
-         $objetivoespecificoAux->addResultadostemplate($objResulesperado);
 
-         $this->em->persist($objResulesperado);
-         $this->em->persist($objetivoespecificoAux);
-         $this->em->flush();
-        
-        
-       // $matrizMensajes = array('El proceso de ingresar Resultado Esperado termino con exito ','Resultado'.$objResulesperado->getIdResEsp());
+    public function agregarResulEsperadoTemplate($resEspeDesc, $resEspNomencl, $resEspIndicador, $idObjetivo) {
+
+        $objetivoespecificoAux = $this->getObjetivoTemplate($idObjetivo);
+
+        $objResulesperado = new ResEspTemplate();
+        $objResulesperado->setResEspTemplDescripcion($resEspeDesc);
+        $objResulesperado->setResEspTemplIndicador($resEspIndicador);
+        $objResulesperado->setIdObjEspecTempl($objetivoespecificoAux);
+
+        $objetivoespecificoAux->addResultadostemplate($objResulesperado);
+
+        $this->em->persist($objResulesperado);
+        $this->em->persist($objetivoespecificoAux);
+        $this->em->flush();
 
         return $objResulesperado->getIdResEspTempl();
     }
-    
-    
+
 }
 
 ?>
